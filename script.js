773d119aba270fc3d31b83fb6811a5ae6a77cdb9
Marcello Júnior <celojuniorii2003@gmail.com>
2024-02-13 19:55:26 -0300

    Alteração

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  let variavellet = 0;
-  const variavelconst = 8;
-
-  variavellet = 5
-  variavelconst = 34
-
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
